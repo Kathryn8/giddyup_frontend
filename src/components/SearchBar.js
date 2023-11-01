78601b10bea3baea4ee63880629a9b806cbf41e8
BryceKnight16 <6373@holbertonstudents.com>
2023-11-01 06:54:46 +0000

    updated api call and searchbar working tripcard

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,156 +1,86 @@
-import React from 'react'
-// import BasicSelect from './BasicSelect'
-import { Box } from '@mui/material';
-// import BasicDatePicker from './BasicDatePicker';
-import Button from '@mui/material/Button';
-// import SearchAllTrips from './SearchAllTrips';
-// import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select'; import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import React, { useState } from 'react';
+import { Box, FormControl, InputLabel, MenuItem, Select, Button } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import axios from 'axios';
 import BookingButton from './BookingButton';
-
-
+import TripCard from './TripCard'
 
 const SearchBar = () => {
-  const [requestBody, setRequestBody] = React.useState([{
-    origin: "",
-    destination: "",
-    deptDate: ""
-  }]);
-  const [origin, setOrigin] = React.useState("");
-  const [destination, setDestination] = React.useState("");
+  const [trips, setTrips] = useState({
+    origin: '',
+    destination: '',
+    deptDate: ''
+  });
 
-  const handleOrigin = (event) => {
-    const updatedRequestBody = { ...requestBody, origin: event.target.value };
-    setRequestBody(updatedRequestBody);
-    setOrigin(event.target.value);
+  const handleSelectChange = (field, value) => {
+    setTrips({ ...trips, [field]: value });
   };
 
-  const handleDestination = (event) => {
-    const updatedRequestBody = { ...requestBody, destination: event.target.value };
-    setRequestBody(updatedRequestBody);
-    setDestination(event.target.value);
-  };
-
-  const handleDate = (date) => {
+  const handleDateChange = (date) => {
     const dateObject = new Date(date);
     dateObject.setHours(dateObject.getHours() - 13);
     const iso8601Date = dateObject.toISOString();
-    // console.log(typeof iso8601Date)
-    const updatedRequestBody = { ...requestBody, deptDate: iso8601Date };
-    // const requestBody = {
-    //   origin: "Yarraville",
-    //   destination: "Melbourne",
-    //   deptDate: "2023-11-26T00:00:00.000Z"
-    // };
-    setRequestBody(updatedRequestBody);
+    setTrips({ ...trips, deptDate: iso8601Date });
+  };
 
-  }
-
-
-
-  // console.log(requestBody);
-
-  const url = 'http://127.0.0.1:5000/api/v1/trips/search';
-  const [trips, setTrips] = React.useState('');
+  const apiUrl = 'http://127.0.0.1:5000/api/v1/trips/search';
+  const [searchedTrips, setSearchedTrips] = useState('');
 
   const searchRequest = async () => {
     try {
-      const { data } = await axios.get(url, {
+      const { data } = await axios.get(apiUrl, {
         headers: {
           Accept: 'application/json',
         },
-        params: requestBody,
+        params: trips,
       });
-      // console.log(data)
-      setTrips(data);
+      setSearchedTrips(data);
     } catch (error) {
       console.log(error.response);
     }
   };
 
-  // try {
-  //   console.log(`Kathryn log: ${trips.data.trips}`);
-  //   const dataList = trips.data.trips;
-  //   console.log(`Datalist: ${dataList[0].name}`) //works now
-  // } catch (error) {
-  //   console.log(error.response);
-  //   console.log("hi");
-  // }
-
-
+  const suburbOptions = ['Ballarat', 'Belgrave', 'Melbourne', 'Seddon', 'Yarraville'];
 
   return (
-    <>
-      <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, alignItems: 'center', justifyContent: 'center', bgcolor: 'white', p: 1.5 }}>
-        <Box sx={{ minWidth: 230, bgcolor: 'white', padding: '3px' }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Origin</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={origin}
-              label="Suburb"
-              onChange={handleOrigin}
-            >
-              <MenuItem value={'Ballarat'}>Ballarat</MenuItem>
-              <MenuItem value={'Belgrave'}>Belgrave</MenuItem>
-              <MenuItem value={'Melbourne'}>Melbourne</MenuItem>
-              <MenuItem value={'Seddon'}>Seddon</MenuItem>
-              <MenuItem value={'Yarraville'}>Yarraville</MenuItem>
-            </Select>
-          </FormControl>
+      <>
+      <div>
+        <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, alignItems: 'center', justifyContent: 'center', bgcolor: 'white', p: 1.5 }}>
+          {['origin', 'destination'].map((field) => (
+            <Box key={field} sx={{ minWidth: 230, bgcolor: 'white', padding: '3px' }}>
+              <FormControl fullWidth>
+                <InputLabel id={`select-label-${field}`}>{field === 'origin' ? 'Origin' : 'Destination'}</InputLabel>
+                <Select
+                  labelId={`select-label-${field}`}
+                  id={`select-${field}`}
+                  value={trips[field]}
+                  label="Suburb"
+                  onChange={(event) => handleSelectChange(field, event.target.value)}
+                >
+                  {suburbOptions.map((suburb) => (
+                    <MenuItem key={suburb} value={suburb}>{suburb}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          ))}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer sx={{ padding: '3px', minWidth: '230px' }} components={['DatePicker']}>
+              <DatePicker label="" onChange={handleDateChange} />
+            </DemoContainer>
+          </LocalizationProvider>
+          <Button onClick={searchRequest} variant="contained" sx={{ margin: '3px', padding: 2, minWidth: { xs: '230px', md: '120px', lg: '230px' }, height: '55px' }} > GiddyUP!</Button>
         </Box>
-        {/* <BasicSelect text='Origin' /> */}
-        <Box sx={{ minWidth: 230, bgcolor: 'white', padding: '3px' }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Destination</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={destination}
-              label="Suburb"
-              onChange={handleDestination}
-            >
-              <MenuItem value={'Ballarat'}>Ballarat</MenuItem>
-              <MenuItem value={'Belgrave'}>Belgrave</MenuItem>
-              <MenuItem value={'Melbourne'}>Melbourne</MenuItem>
-              <MenuItem value={'Seddon'}>Seddon</MenuItem>
-              <MenuItem value={'Yarraville'}>Yarraville</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        {/* <BasicSelect text='Destination' /> */}
-        {/* <BasicDatePicker /> */}
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer sx={{ padding: '3px', minWidth: '230px' }} components={['DatePicker']}>
-            <DatePicker label="" onChange={handleDate} />
-          </DemoContainer>
-        </LocalizationProvider>
-        <Button onClick={searchRequest} variant="contained" sx={{ margin: '3px', padding: 2, minWidth: { xs: '230px', md: '120px', lg: '230px' }, height: '55px' }} > GiddyUP!</Button>
-      </Box>
-
-      {/* hard code values for "A fun commute": */}
-      <BookingButton tripId='65390389168ea9d1620f988b' passengerId='65388a9f6b835a3128e2d24c' />
-
-      {/* // testing::: */}
-      {/* <SearchAllTrips /> / Random joke button */}
-      {/* {trips.data.trips.map((x) => {
-        <ul>
-          <li>{trips.name}</li>
-          <li>{trips.origin}</li>
-          <li>{trips.destination}</li>
-          <li>{trips.deptDate}</li>
-        </ul >
-      })} */}
-    </>
-  )
-}
+        <BookingButton tripId='65390389168ea9d1620f988b' passengerId='65388a9f6b835a3128e2d24c' />
+      </div>
+      {searchedTrips && searchedTrips.data.trips.map((trip) => (
+        <TripCard key={trip.id} trip={trip} />
+      ))}
+      </>
+  );
+};
 
 export default SearchBar;
--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,52 +1,23 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import TripInfo from '../TripInfo';
+import { Card, CardContent, Typography, CardActions } from '@mui/material';
+import BookingButton from './BookingButton';
 
-const TripCard = ({ tripInfo }) => {
-  const cardStyle = {
-    border: '1px solid #ccc',
-    padding: '16px',
-    maxWidth: '600px',
-    margin: '16px auto',
-  };
-    const textStyles = {
-    margin: '2px 0',
-  };
-
-
+const TripCard = ({ trip }) => {
+  const { name, origin, destination, deptDate } = trip;
 
   return (
-    <Box style={cardStyle}>
-      <div>
-        <h3 style={textStyles}>{tripInfo.title}</h3>
-        <p style={textStyles}>Origin: {tripInfo.origin}</p>
-        <p style={textStyles}>Destination: {tripInfo.destination}</p>
-        <p style={textStyles}>Departure Date: {tripInfo.deptDate}</p>
-      </div>
-      <Grid container justifyContent="flex-end">
-        <Button variant="contained" color="primary">
-          Book Now
-        </Button>
-        <Button variant="outlined" color="primary">
-          Details
-        </Button>
-      </Grid>
-    </Box>
+    <Card sx={{ marginBottom: 2 }}>
+      <CardContent>
+        <Typography variant="h5">{name}</Typography>
+        <Typography>Origin: {origin}</Typography>
+        <Typography>Destination: {destination}</Typography>
+        <Typography>Departure Date: {deptDate}</Typography>
+      </CardContent>
+      <CardActions sx={{ padding: 2 }}>
+      <BookingButton/>
+      </CardActions>
+    </Card>
   );
 };
 
-const App = () => {
-  return (
-    <div>
-      <TripCard tripInfo={TripInfo.tripInfo1} /> 
-      <TripCard tripInfo={TripInfo.tripInfo2} />
-      <TripCard tripInfo={TripInfo.tripInfo3} />
-      <TripCard tripInfo={TripInfo.tripInfo4} />
-      <TripCard tripInfo={TripInfo.tripInfo5} />
-    </div>
-  );
-};
-
-export default App;
+export default TripCard;
