4666f80255a2cc030a9b1fab9c46a871eb08f20e
BryceKnight16 <6373@holbertonstudents.com>
2023-11-03 04:19:55 +0000

    Some styles to the trip card no 100% correct

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -9,8 +9,16 @@ import DialogTitle from '@mui/material/DialogTitle';
 import TripCardSimplified from './TripCardSimplified';
 
 const TripCard = ({ trip, userId }) => {
-  const { name, origin, destination, deptDate, _id: tripId } = trip;
-
+  const { name, origin, destination, deptDate, deptDateTime, _id: tripId } = trip;
+  const cardStyle = {
+    border: '1px solid #ccc',
+    padding: '16px',
+    maxWidth: '600px',
+    margin: '16px auto',
+  };
+  const textStyles = {
+    margin: '2px 0',
+  };
   const [openAlertDialog, setOpenAlertDialog] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -23,12 +31,14 @@ const TripCard = ({ trip, userId }) => {
 
 
   return (
-    <Card sx={{ marginBottom: 2 }}>
+    <Card style ={cardStyle} sx={{ marginBottom: 2 }}>
       <CardContent>
         <Typography variant="h5">{name}</Typography>
-        <Typography>Origin: {origin}</Typography>
-        <Typography>Destination: {destination}</Typography>
-        <Typography>Departure Date: {deptDate}</Typography>
+        <Typography style={textStyles}>Origin: {origin}</Typography>
+        <Typography style={textStyles}>Destination: {destination}</Typography>
+        <Typography style={textStyles}>Departure Date: {deptDate}</Typography>
+        <Typography>Departure Time: {deptDateTime}</Typography>
+        <Typography style={textStyles}>Driver Name: George 4.6</Typography>
       </CardContent>
       <CardActions sx={{ padding: 2 }}>
         <Button variant="contained" onClick={handleClickOpen}>
