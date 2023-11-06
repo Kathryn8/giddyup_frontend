08f247d5bd225a971614c1db7b9c8c66678687aa
BryceKnight16 <6373@holbertonstudents.com>
2023-11-06 23:51:24 +0000

    updating trip card into grids and dane and time

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import { Card, CardContent, Typography, CardActions, Button, Grid } from '@mui/material';
 import BookingButton from './BookingButton';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -15,10 +15,9 @@ const TripCard = ({ trip, userId }) => {
     padding: '16px',
     maxWidth: '600px',
     margin: '16px auto',
+    
   };
-  const textStyles = {
-    margin: '2px 0',
-  };
+    
   const [openAlertDialog, setOpenAlertDialog] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -34,18 +33,61 @@ const TripCard = ({ trip, userId }) => {
     window.location = "/dashboard";
   };
 
+  
+  const apiDate = deptDate;
+  const date = new Date(apiDate);
+  const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+
+const apiTime = deptDateTime; // Assuming deptDateTime is in the format 'YYYY-MM-DDTHH:mm:ss.sssZ'
+const time = new Date(apiTime);
+
+const hours = time.getUTCHours();
+const minutes = time.getUTCMinutes();
+const seconds = time.getUTCSeconds();
+const ampm = hours >= 12 ? 'PM' : 'AM';
+
+let formattedTime;
+
+if (hours === 0) {
+  formattedTime = `${minutes}:${seconds} ${ampm}`;
+} else {
+  formattedTime = `${hours % 12}:${minutes}:${seconds} ${ampm}`;
+}
 
   return (
     <Card style={cardStyle} sx={{ marginBottom: 2 }}>
       <CardContent>
-        <Typography variant="h5">{name}</Typography>
-        <Typography style={textStyles}>Origin: {origin}</Typography>
-        <Typography style={textStyles}>Destination: {destination}</Typography>
-        <Typography style={textStyles}>Departure Date: {deptDate}</Typography>
-        <Typography>Departure Time: {deptDateTime}</Typography>
-        <Typography style={textStyles}>Driver Name: George 4.6</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h3">{name}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" style={{ color: 'black' }}>
+              Origin: {origin}
+            </Typography>
+            {/* <img src={require('../assets/images/startandend.png')} alt="Origin to Destination" /> */}
+            <Typography variant="body1" style={{ color: 'black' }}>
+              Destination: {destination}
+            </Typography>
+            <Typography variant="body1" style={{ color: 'black' }}>
+              Driver Name: George 4.6
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1" style={{ color: 'black' }}>
+              Departure Date: {formattedDate}
+            </Typography>
+            <Typography variant="body1" style={{ color: 'black' }}>
+              Departure Time: {formattedTime}
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
       <CardActions sx={{ padding: 2 }}>
+        <Button sx ={{alignItems: 'Right'}} variant="contained" >
+          Learn more about your Driver
+        </Button>
         <Button variant="contained" onClick={handleClickOpen}>
           Book this trip now
         </Button>
